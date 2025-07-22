a1a50e9e57ec44365e9e75235776a95b4b78a769
Walter Garay <waltergaraycr@gmail.com>
2025-07-22 11:48:13 -0500

    Se añadió la configuración para forzar el uso de HTTPS en producción en el 'AppServiceProvider', mejorando así la seguridad de la aplicación.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
