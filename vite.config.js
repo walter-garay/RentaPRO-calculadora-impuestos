a95eb605fbb57944052e670558c49059d41658cf
Walter Garay <waltergaraycr@gmail.com>
2025-07-22 11:59:37 -0500

    Actualización de la configuración de Vite para establecer la propiedad 'base' en función del entorno, utilizando una URL de producción para despliegues en producción y manteniendo '/build/' para entornos de desarrollo.

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-    base: '/build/',
+    base: isProduction ? 'https://rentapro.onrender.com/build/' : '/build/',
     plugins: [
         laravel({
             input: 'resources/js/app.js',
